f16448a248542bd036ee6320afb79efc5cc626fc
cagdas <cakaraca@netas.com.tr>
2021-07-13 10:22:20 +0300

    fix

--- FSD Sample Project App/Controllers/HomeController.cs
+++ FSD Sample Project App/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FSD_Sample_Project_App.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,56 @@ namespace FSD_Sample_Project_App.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+
+        FSDSampleProjectdbEntities db = new FSDSampleProjectdbEntities();
+        public ActionResult Index(string ara)
+        {
+            var model = db.Urun.ToList();
+            if (!string.IsNullOrEmpty(ara))
+            {
+                model = model.Where(x => x.KlinikAdi.Contains(ara) || x.EkipmanAdi.Contains(ara)).ToList();
+            }
+            return View(model);
+        }
+        public ActionResult Ekle()
         {
             return View();
         }
-
-        public ActionResult About()
+        public ActionResult Ekle2(Urun k)
         {
-            ViewBag.Message = "Your application description page.";
+            db.Urun.Add(k);
+            db.SaveChanges();
+            return RedirectToAction("Index");
 
-            return View();
         }
-
-        public ActionResult Contact()
+        public ActionResult GuncelleBilgiGetir(Urun k)
         {
-            ViewBag.Message = "Your contact page.";
+            var model = db.Urun.Find(k.ID);
+            if (model == null) return HttpNotFound();
+            return View(model);
 
-            return View();
+        }
+        public ActionResult Guncelle(Urun k)
+        {
+            db.Entry(k).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+        public ActionResult SilBilgiGetir(Urun k)
+        {
+            var model = db.Urun.Find(k.ID);
+            if (model == null) return HttpNotFound();
+            return View(model);
+
+        }
+        public ActionResult Sil(Urun k)
+        {
+            db.Entry(k).State = System.Data.Entity.EntityState.Deleted;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
         }
     }
 }
--- FSD Sample Project App/Global.asax.cs
+++ FSD Sample Project App/Global.asax.cs
@@ -15,7 +15,7 @@ namespace FSD_Sample_Project_App
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
