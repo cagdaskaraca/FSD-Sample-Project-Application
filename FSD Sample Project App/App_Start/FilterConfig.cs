c603208c80811dcc1d366f196a348039d756b6cf
cagdas <cakaraca@netas.com.tr>
2021-07-13 09:46:00 +0300

    Add project files.

---
+++ FSD Sample Project App/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FSD_Sample_Project_App
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ FSD Sample Project App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FSD_Sample_Project_App.MvcApplication" Language="C#" %>
